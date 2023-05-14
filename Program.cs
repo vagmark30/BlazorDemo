b5e6b37c1c03ab65ace66dcb263b9a30a75c6a84
vagmark <vagmark30@gmail.com>
2023-05-14 18:06:26 +0300

    EM Share data in Blazor applications

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
 using BlazingPizza.Data;
+using BlazingPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-// builder.Services.AddSingleton<PizzaService>();
+builder.Services.AddScoped<OrderState>();
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
 
